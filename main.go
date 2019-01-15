99fa19644badeb0f1b4ce4dcbd475e2efd85d617
CJ Enright <cenrigh@purdue.edu>
2019-01-15 15:28:37 -0500

    Removes global projects

--- main.go
+++ main.go
@@ -12,47 +12,75 @@ import (
 	"time"
 )
 
-var (
-	projects []Project
-)
-
-// TODO Maybe have some config in .tb.json? like l10n for military/ISO dates, format strings
+// TODO Maybe have some config in .tb.json? like l10n for 24h/ISO dates, format strings
+// TODO stats and status
+// TODO ideally we shouldn't be reading the whole json file on every operation
+// because most of them will just be starting/stopping.  The only time we do
+// need to read operations is when we're calculating times.
 func main() {
-	var err error
-
 	path := os.Getenv("HOME") + "/.tb.json"
-	err = load(path)
+	projects, err := loadProjects(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	l := len(os.Args)
 	if l == 1 {
-		Status()
+		Status(projects)
 	} else if l == 3 {
-		command := os.Args[1]
+		// ToLower will make commands case insensitive
+		command := strings.ToLower(os.Args[1])
 		projectName := os.Args[2]
 
-		// TODO make commands case insensitive
-		// TODO I don't like how we deal with projectName, the same thing is repeated in like every command func
+		var p *Project
+		p, err = FindProject(projects, projectName)
+
 		if command == "new" {
-			err = New(projectName)
-		} else if command == "start" {
-			err = Start(projectName)
-		} else if command == "stop" {
-			err = Stop(projectName)
-		} else if command == "timecard" {
-			err = Timecard(projectName)
-		} else if command == "archive" {
-			err = Archive(projectName)
-		} else if command == "s" {
-			// TODO this one should be a toggle
+			if err == nil && p.Name == projectName {
+				fmt.Printf("project with name \"%s\" already exists\n", p.Name)
+				return
+			} else {
+				err = nil
+
+				p := Project{Name: projectName}
+				projects = append(projects, p)
+				fmt.Printf("created project \"%s\"\n", p.Name)
+			}
+		}
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		switch command {
+		case "start":
+			err = p.Start()
+		case "stop":
+			err = p.Stop()
+		case "s":
+			if p.Active == true {
+				p.Stop()
+			} else {
+				p.Start()
+			}
+		case "timecard":
+			err = p.PrintTimecard()
+		case "archive":
+			p.Archive()
 		}
 	}
+
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
-	err = save(path)
+	err = saveProjects(path, projects)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 }
 
@@ -74,56 +102,48 @@ type Project struct {
 	Entries   []Entry   `json:"segments"`
 }
 
-func (p *Project) EntriesSince(t time.Time) (entries []Entry) {
-	for _, s := range p.Entries {
-		if t.Before(s.Start) {
-			entries = append(entries, s)
-		}
+func (p *Project) Start() (err error) {
+	if p.Active {
+		return fmt.Errorf("project \"%s\" is already running", p.Name)
+	} else {
+		e := Entry{Start: time.Now()}
+		p.Entries = append(p.Entries, e)
+		p.Active = true
+
+		fmt.Printf("started \"%s\" at %s\n", p.Name, e.Start.Format("15:04 EDT"))
 	}
 
-	return entries
+	return err
 }
 
-func (p *Project) DurationSince(t time.Time) (d time.Duration) {
-	// Calculate own time
-	for _, s := range p.Entries {
-		if s.End.IsZero() {
-			// If something hasn't ended, just count the time from start to now
-			d += time.Now().Sub(s.Start)
-		} else if t.Before(s.Start) {
-			d += s.Duration
-		}
+func (p *Project) Stop() (err error) {
+	if p.Active {
+		p.Entries[len(p.Entries)-1].End = time.Now()
+		p.Entries[len(p.Entries)-1].FindDuration()
+		p.Active = false
+
+		dur := p.Entries[len(p.Entries)-1].Duration
+		fmt.Printf("stopped \"%s\" after a duration of %s\n", p.Name, dur.Truncate(time.Second).String())
+	} else {
+		return fmt.Errorf("project \"%s\" isn't running", p.Name)
 	}
 
-	// Now calculate time of all children (projects with name p.Name/*)
-	for _, c := range projects {
-		if strings.Contains(c.Name, p.Name+"/") {
-			d += c.DurationSince(t)
-		}
-	}
-
-	return d
+	return err
 }
 
-func Timecard(projectName string) (err error) {
-	/*
-		Date 	Start End   Duration
-		11/2  11:45 13:22 45m32s
-		11/2  11:45 13:22 45m32s
+func (p *Project) Archive() {
+	p.Active = false
+	p.Archived = true
 
-		Total: 3hr2min over the past week
-	*/
-	index, err := FindProjectIndex(projectName)
-	if err != nil {
-		return err
-	}
-	p := projects[index]
+	fmt.Printf("archived \"%s\"\n", p.Name)
+}
 
+func (p *Project) PrintTimecard() (err error) {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
 	fmt.Fprintf(w, "Date\tStart\tEnd\tDuration\n")
 
 	// TODO user inputted time range
-	entries := p.EntriesSince(time.Now().AddDate(0, 0, -7))
+	entries := p.entriesSince(time.Now().AddDate(0, 0, -7))
 	for _, e := range entries {
 		date := strconv.Itoa(int(e.Start.Month())) + "/" + strconv.Itoa(e.Start.Day())
 		// TODO these things are wacci af
@@ -143,12 +163,46 @@ func Timecard(projectName string) (err error) {
 
 	fmt.Println("---------------------------------------")
 	// TODO user inputted time range
-	durSince := p.DurationSince(time.Now().AddDate(0, 0, -7)).Truncate(time.Second)
+	durSince := p.durationSince(time.Now().AddDate(0, 0, -7)).Truncate(time.Second)
+	// TODO have this say how far back duration goes
 	fmt.Printf("Total duration: %s\n", durSince)
 	return err
 }
 
-func Status() {
+func (p *Project) entriesSince(t time.Time) (entries []Entry) {
+	for _, s := range p.Entries {
+		if t.Before(s.Start) {
+			entries = append(entries, s)
+		}
+	}
+
+	return entries
+}
+
+func (p *Project) durationSince(t time.Time) (d time.Duration) {
+	// Calculate own time
+	for _, s := range p.Entries {
+		if s.End.IsZero() {
+			// If something hasn't ended, just count the time from start to now
+			d += time.Now().Sub(s.Start)
+		} else if t.Before(s.Start) {
+			d += s.Duration
+		}
+	}
+
+	/*
+		// Now calculate time of all children (projects with name p.Name/*)
+		for _, c := range projects {
+			if strings.Contains(c.Name, p.Name+"/") {
+				d += c.durationSince(t)
+			}
+		}
+	*/
+
+	return d
+}
+
+func Status(projects []Project) {
 	// TODO fix all this ish
 	for _, p := range projects {
 		name := p.Name
@@ -178,13 +232,14 @@ func Status() {
 	}
 }
 
-// TODO make stats and status different
+// TODO clean this up
+/*
 func Stats(projectName string) (err error) {
 	if len(os.Args) < 3 {
 	} else if len(os.Args) == 3 {
 		if os.Args[2] == "day" {
 			for _, p := range projects {
-				dur := p.DurationSince(time.Now().AddDate(0, 0, -1))
+				dur := p.durationSince(time.Now().AddDate(0, 0, -1))
 				fmt.Printf(
 					"logged %s for %s in the past day\n",
 					p.Name,
@@ -193,7 +248,7 @@ func Stats(projectName string) (err error) {
 			}
 		} else if os.Args[2] == "week" {
 			for _, p := range projects {
-				dur := p.DurationSince(time.Now().AddDate(0, 0, -7))
+				dur := p.durationSince(time.Now().AddDate(0, 0, -7))
 				fmt.Printf(
 					"logged %s for %s in the past week\n",
 					p.Name,
@@ -202,7 +257,7 @@ func Stats(projectName string) (err error) {
 			}
 		} else if os.Args[2] == "month" {
 			for _, p := range projects {
-				dur := p.DurationSince(time.Now().AddDate(0, -1, 0))
+				dur := p.durationSince(time.Now().AddDate(0, -1, 0))
 				fmt.Printf(
 					"logged %s for %s in the past month\n",
 					p.Name,
@@ -214,132 +269,40 @@ func Stats(projectName string) (err error) {
 
 	return err
 }
+*/
 
-func New(projectName string) (err error) {
-	index := IndexOf(projects, projectName)
-	if index != -1 {
-		return fmt.Errorf("project with name \"%s\" already exists", projectName)
-	}
+// FindProject finds the first full match or suffix match of a project
+// TODO check for collisions when suffix matching?
+func FindProject(projects []Project, projectName string) (project *Project, err error) {
+	index := -1
 
-	p := Project{Name: projectName}
-	projects = append(projects, p)
-
-	fmt.Printf("created project \"%s\"\n", p.Name)
-
-	return err
-}
-
-func Start(projectName string) (err error) {
-	index, err := FindProjectIndex(projectName)
-	if err != nil {
-		return err
-	}
-
-	projectName = projects[index].Name
-
-	if projects[index].Active {
-		return fmt.Errorf("project \"%s\" is already running", projectName)
-	} else {
-		e := Entry{Start: time.Now()}
-		projects[index].Entries = append(projects[index].Entries, e)
-		projects[index].Active = true
-
-		fmt.Printf("started \"%s\" at %s\n", projectName, e.Start.Format("15:04 EDT"))
-	}
-
-	return err
-}
-
-func Stop(projectName string) (err error) {
-	index, err := FindProjectIndex(projectName)
-	if err != nil {
-		return err
-	}
-
-	projectName = projects[index].Name
-
-	if projects[index].Active {
-		projects[index].Entries[len(projects[index].Entries)-1].End = time.Now()
-		projects[index].Entries[len(projects[index].Entries)-1].FindDuration()
-		projects[index].Active = false
-
-		dur := projects[index].Entries[len(projects[index].Entries)-1].Duration
-		fmt.Printf("stopped \"%s\" after a duration of %s\n", projects[index].Name, dur.Truncate(time.Second).String())
-	} else {
-		return fmt.Errorf("project \"%s\" isn't running", projectName)
-	}
-
-	return err
-}
-
-func Archive(projectName string) (err error) {
-	index, err := FindProjectIndex(projectName)
-	if err != nil {
-		return err
-	}
-
-	projectName = projects[index].Name
-
-	projects[index].Active = false
-	projects[index].Archived = true
-	fmt.Printf("Archived \"%s\"\n", projectName)
-
-	return err
-}
-
-func IndexOf(a []Project, s string) (i int) {
-	for i, e := range a {
-		if e.Name == s {
-			return i
+	for i, e := range projects {
+		if e.Name == projectName {
+			index = i
+			break
+		} else if strings.HasSuffix(e.Name, projectName) && index == -1 {
+			index = i
 		}
 	}
 
-	return -1
-}
-
-func IndexOfSuffix(a []Project, s string) (i int) {
-	for i, e := range a {
-		if strings.HasSuffix(e.Name, s) {
-			return i
-		}
-	}
-
-	return -1
-}
-
-func Contains(s []string, e string) bool {
-	for _, v := range s {
-		if v == e {
-			return true
-		}
-	}
-
-	return false
-}
-
-func FindProjectIndex(projectName string) (index int, err error) {
-	index = IndexOf(projects, projectName)
 	if index == -1 {
-		index = IndexOfSuffix(projects, projectName)
-	}
-	if index == -1 {
-		return index, fmt.Errorf("no project named \"%s\" found", projectName)
+		return project, fmt.Errorf("no project named \"%s\" found", projectName)
 	}
 
-	return index, nil
+	return &projects[index], err
 }
 
-func load(path string) (err error) {
+func loadProjects(path string) (projects []Project, err error) {
 	input, err := ioutil.ReadFile(path)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	err = json.Unmarshal(input, &projects)
-	return err
+	return projects, err
 }
 
-func save(path string) (err error) {
+func saveProjects(path string, projects []Project) (err error) {
 	// Always sort projects by name before writing them
 	sort.Slice(projects, func(i, j int) bool {
 		return projects[i].Name < projects[j].Name
@@ -352,4 +315,14 @@ func save(path string) (err error) {
 
 	err = ioutil.WriteFile(path, out, 0644)
 	return err
+}
+
+func Contains(s []string, e string) bool {
+	for _, v := range s {
+		if v == e {
+			return true
+		}
+	}
+
+	return false
 }
